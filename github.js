23410b7a82844111678df1fe4c8b25cd817814d4
Tomas Brambora <tomas@canva.com>
2018-09-25 22:54:41 +1000

    WIP

--- github.js
+++ github.js
@@ -7,136 +7,133 @@ const colors = require('colors');
 const emoji = require('node-emoji');
 
 async function constructPrMsg(sg, branch) {
-    const title = await sg.raw(['log', '--format=%s', '-n', '1', branch]);
-    const body = await sg.raw(['log', '--format=%b', '-n', '1', branch]);
-    return {
-        title: title.trim(),
-        body: body.trim(),
-    };
+  const title = await sg.raw(['log', '--format=%s', '-n', '1', branch]);
+  const body = await sg.raw(['log', '--format=%b', '-n', '1', branch]);
+  return {
+    title: title.trim(),
+    body: body.trim(),
+  };
 }
 
 function constructTrainNavigation(branchToPrDict, currentBranch, combinedBranch) {
-    let contents = '#### PR chain:\n';
-    return Object.keys(branchToPrDict).reduce((output, branch) => {
-        const maybeHandRight = branch === currentBranch ? '👉 ' : '';
-        const maybeHandLeft = branch === currentBranch ? ' 👈 **YOU ARE HERE**' : '';
-        const combinedInfo = branch === combinedBranch ? ' **[combined branch]** ' : ' ';
-        output += `${maybeHandRight}#${branchToPrDict[branch].pr}${combinedInfo}(${branchToPrDict[
-            branch
-        ].title.trim()})${maybeHandLeft}`;
-        return output + '\n';
-    }, contents);
+  let contents = '#### PR chain:\n';
+  return Object.keys(branchToPrDict).reduce((output, branch) => {
+    const maybeHandRight = branch === currentBranch ? '👉 ' : '';
+    const maybeHandLeft = branch === currentBranch ? ' 👈 **YOU ARE HERE**' : '';
+    const combinedInfo = branch === combinedBranch ? ' **[combined branch]** ' : ' ';
+    output += `${maybeHandRight}#${branchToPrDict[branch].pr}${combinedInfo}(${branchToPrDict[
+      branch
+    ].title.trim()})${maybeHandLeft}`;
+    return output + '\n';
+  }, contents);
 }
 
 function readGHKey() {
-    return fs
-        .readFileSync(`${process.env.HOME}/.pr-train`, 'UTF-8')
-        .toString()
-        .trim();
+  return fs
+    .readFileSync(`${process.env.HOME}/.pr-train`, 'UTF-8')
+    .toString()
+    .trim();
 }
 
 async function ensurePrsExist(sg, allBranches, combinedBranch, remote = DEFAULT_REMOTE) {
-    //const allBranches = combinedBranch ? sortedBranches.concat(combinedBranch) : sortedBranches;
-    const octoClient = octo.client(readGHKey());
-    // TODO: take remote name from `-r` value.
-    const remoteUrl = await sg.raw(['config', '--get', `remote.${remote}.url`]);
-    if (!remoteUrl) {
-        console.log(`URL for remote ${remote} not found in your git config`.red);
-        process.exit(4);
-    }
+  //const allBranches = combinedBranch ? sortedBranches.concat(combinedBranch) : sortedBranches;
+  const octoClient = octo.client(readGHKey());
+  // TODO: take remote name from `-r` value.
+  const remoteUrl = await sg.raw(['config', '--get', `remote.${remote}.url`]);
+  if (!remoteUrl) {
+    console.log(`URL for remote ${remote} not found in your git config`.red);
+    process.exit(4);
+  }
 
-    let combinedBranchTitle;
-    if (combinedBranch) {
-        console.log();
-        console.log(`Now I will need to know what to call your "combined" branch PR in GitHub.`);
-        combinedBranchTitle = await promptly.prompt(colors.bold(`Combined branch PR title:`));
-        if (!combinedBranchTitle) {
-            console.log(
-                `Cannot continue.`.red,
-                `(I need to know what the title of your combined branch PR should be.)`
-            );
-            process.exit(5);
-        }
+  let combinedBranchTitle;
+  if (combinedBranch) {
+    console.log();
+    console.log(`Now I will need to know what to call your "combined" branch PR in GitHub.`);
+    combinedBranchTitle = await promptly.prompt(colors.bold(`Combined branch PR title:`));
+    if (!combinedBranchTitle) {
+      console.log(`Cannot continue.`.red, `(I need to know what the title of your combined branch PR should be.)`);
+      process.exit(5);
     }
+  }
 
-    const getCombinedBranchPrMsg = () => ({
-        title: combinedBranchTitle,
-        body: '',
+  const getCombinedBranchPrMsg = () => ({
+    title: combinedBranchTitle,
+    body: '',
+  });
+
+  console.log();
+  console.log('This will create (or update) PRs for the following branches:');
+  await allBranches.reduce(async (memo, branch) => {
+    await memo;
+    const { title } = branch === combinedBranch ? getCombinedBranchPrMsg() : await constructPrMsg(sg, branch);
+    console.log(`  -> ${branch.green} (${title.italic})`);
+  }, Promise.resolve());
+
+  console.log();
+  if (!(await promptly.confirm(colors.bold('Shall we do this? [y/n] ')))) {
+    console.log('No worries. Bye now.', emoji.get('wave'));
+    process.exit(0);
+  }
+
+  const nickAndRepo = remoteUrl.match(/:(.*)\.git/)[1];
+  if (!nickAndRepo) {
+    console.log(`I could not parse your remote ${remote} repo URL`.red);
+    process.exit(4);
+  }
+
+  const nick = nickAndRepo.split('/')[0];
+  const ghRepo = octoClient.repo(nickAndRepo);
+
+  console.log('');
+  // Construct branch_name <-> PR_data mapping.
+  // Note: We're running this serially to have nicer logs.
+  const prDict = await allBranches.reduce(async (_memo, branch, index) => {
+    const memo = await _memo;
+    const { title, body } = branch === combinedBranch ? getCombinedBranchPrMsg() : await constructPrMsg(sg, branch);
+    const base = index === 0 || branch === combinedBranch ? 'master' : allBranches[index - 1];
+    process.stdout.write(`Checking if PR for branch ${branch} already exists... `);
+    const prs = await ghRepo.prsAsync({
+      head: `${nick}:${branch}`,
     });
-
-    console.log();
-    console.log('This will create (or update) PRs for the following branches:');
-    await allBranches.reduce(async (memo, branch) => {
-        await memo;
-        const { title } = branch === combinedBranch ? getCombinedBranchPrMsg() : await constructPrMsg(sg, branch);
-        console.log(`  -> ${branch.green} (${title.italic})`);
-    }, Promise.resolve());
-
-    console.log();
-    if (!(await promptly.confirm(colors.bold('Shall we do this? [y/n] ')))) {
-        console.log('No worries. Bye now.', emoji.get('wave'));
-        process.exit(0);
+    let prResponse = prs[0] && prs[0][0];
+    if (prResponse) {
+      console.log('yep');
+    } else {
+      console.log('nope');
+      const payload = {
+        head: branch,
+        base,
+        title,
+        body,
+      };
+      process.stdout.write(`Creating PR for branch "${branch}"...`);
+      prResponse = (await ghRepo.prAsync(payload))[0];
+      console.log(emoji.get('white_check_mark'));
     }
+    memo[branch] = {
+      title,
+      pr: prResponse.number,
+    };
+    return memo;
+  }, Promise.resolve({}));
 
-    const nickAndRepo = remoteUrl.match(/:(.*)\.git/)[1];
-    if (!nickAndRepo) {
-        console.log(`I could not parse your remote ${remote} repo URL`.red);
-        process.exit(4);
-    }
-
-    const nick = nickAndRepo.split('/')[0];
-    const ghRepo = octoClient.repo(nickAndRepo);
-
-    console.log('');
-    // Construct branch_name <-> PR_data mapping.
-    // Note: We're running this serially to have nicer logs.
-    const prDict = await allBranches.reduce(async (_memo, branch, index) => {
-        const memo = await _memo;
-        const { title, body } = branch === combinedBranch ? getCombinedBranchPrMsg() : await constructPrMsg(sg, branch);
-        const base = index === 0 || branch === combinedBranch ? 'master' : allBranches[index - 1];
-        process.stdout.write(`Checking if PR for branch ${branch} already exists... `);
-        const prs = await ghRepo.prsAsync({
-            head: `${nick}:${branch}`,
-        });
-        let prResponse = prs[0] && prs[0][0];
-        if (prResponse) {
-            console.log('yep');
-        } else {
-            console.log('nope');
-            const payload = {
-                head: branch,
-                base,
-                title,
-                body,
-            };
-            process.stdout.write(`Creating PR for branch "${branch}"...`);
-            prResponse = (await ghRepo.prAsync(payload))[0];
-            console.log(emoji.get('white_check_mark'));
-        }
-        memo[branch] = {
-            title,
-            pr: prResponse.number,
-        };
-        return memo;
-    }, Promise.resolve({}));
-
-    // Now that we have all the PRs, let's update them with the "navigation" section.
-    // Note: We're running this serially to have nicer logs.
-    await allBranches.reduce(async (memo, branch) => {
-        await memo;
-        const ghPr = octoClient.pr(nickAndRepo, prDict[branch].pr);
-        const { title, body } = branch === combinedBranch ? getCombinedBranchPrMsg() : await constructPrMsg(sg, branch);
-        const navigation = constructTrainNavigation(prDict, branch, combinedBranch);
-        process.stdout.write(`Updating PR for branch ${branch}...`);
-        await ghPr.updateAsync({
-            title,
-            body: `${body}\n${navigation}`,
-        });
-        console.log(emoji.get('white_check_mark'));
-    }, Promise.resolve());
+  // Now that we have all the PRs, let's update them with the "navigation" section.
+  // Note: We're running this serially to have nicer logs.
+  await allBranches.reduce(async (memo, branch) => {
+    await memo;
+    const ghPr = octoClient.pr(nickAndRepo, prDict[branch].pr);
+    const { title, body } = branch === combinedBranch ? getCombinedBranchPrMsg() : await constructPrMsg(sg, branch);
+    const navigation = constructTrainNavigation(prDict, branch, combinedBranch);
+    process.stdout.write(`Updating PR for branch ${branch}...`);
+    await ghPr.updateAsync({
+      title,
+      body: `${body}\n${navigation}`,
+    });
+    console.log(emoji.get('white_check_mark'));
+  }, Promise.resolve());
 }
 
 module.exports = {
-    ensurePrsExist,
-    readGHKey,
+  ensurePrsExist,
+  readGHKey,
 };
--- index.js
+++ index.js
@@ -6,54 +6,61 @@ const difference = require('lodash.difference');
 const program = require('commander');
 const ProgressBar = require('progress');
 const emoji = require('node-emoji');
-const package = require('./package.json');
 const fs = require('fs');
 const yaml = require('js-yaml');
 const { ensurePrsExist, readGHKey } = require('./github');
 const colors = require('colors');
 const { DEFAULT_REMOTE, MERGE_STEP_DELAY_MS, MERGE_STEP_DELAY_WAIT_FOR_LOCK } = require('./consts');
+const path = require('path');
+// @ts-ignore
+const package = require('./package.json');
 
 const sleep = ms => new Promise(resolve => setTimeout(resolve, ms));
 
 async function combineBranches(sg, rebase, from, to) {
-    if (program.rebase) {
-        process.stdout.write(`rebasing ${to} onto branch ${from}... `);
-    } else {
-        process.stdout.write(`merging ${from} into branch ${to}... `);
+  if (program.rebase) {
+    process.stdout.write(`rebasing ${to} onto branch ${from}... `);
+  } else {
+    process.stdout.write(`merging ${from} into branch ${to}... `);
+  }
+  try {
+    await sg.checkout(to);
+    (await rebase) ? sg.rebase([from]) : sg.merge([from]);
+  } catch (e) {
+    if (!e.conflicts || e.conflicts.length === 0) {
+      await sleep(MERGE_STEP_DELAY_WAIT_FOR_LOCK);
+      await sg.checkout(to);
+      (await rebase) ? sg.rebase([from]) : sg.merge([from]);
     }
-    try {
-        await sg.checkout(to);
-        (await rebase) ? sg.rebase([from]) : sg.merge([from]);
-    } catch (e) {
-        if (!e.conflicts || e.conflicts.length === 0) {
-            await sleep(MERGE_STEP_DELAY_WAIT_FOR_LOCK);
-            await sg.checkout(to);
-            (await rebase) ? sg.rebase([from]) : sg.merge([from]);
-        }
-    }
-    console.log(emoji.get('white_check_mark'));
+  }
+  console.log(emoji.get('white_check_mark'));
 }
 
 async function pushChanges(sg, branches, forcePush, remote = DEFAULT_REMOTE) {
-    console.log(`Pushing changes to remote ${remote}...`);
-    const bar = new ProgressBar('Pushing [:bar] :percent :elapsed', {
-        width: 20,
-        total: branches.length + 1,
-        clear: true,
-    });
-    bar.tick(1);
-    const promises = branches.map(b => sg.push(remote, b, { '--force': forcePush }).then(() => bar.tick(1)));
-    await Promise.all(promises);
-    console.log('All changes pushed ' + emoji.get('white_check_mark'));
+  console.log(`Pushing changes to remote ${remote}...`);
+  const bar = new ProgressBar('Pushing [:bar] :percent :elapsed', {
+    width: 20,
+    total: branches.length + 1,
+    clear: true,
+  });
+  bar.tick(1);
+  const promises = branches.map(b => sg.push(remote, b, { '--force': forcePush }).then(() => bar.tick(1)));
+  await Promise.all(promises);
+  console.log('All changes pushed ' + emoji.get('white_check_mark'));
 }
 
 async function getUnmergedBranches(sg, branches) {
-    const mergedBranchesOutput = await sg.raw(['branch', '--merged', 'master']);
-    const mergedBranches = mergedBranchesOutput
-        .split('\n')
-        .map(b => b.trim())
-        .filter(Boolean);
-    return difference(branches, mergedBranches);
+  const mergedBranchesOutput = await sg.raw(['branch', '--merged', 'master']);
+  const mergedBranches = mergedBranchesOutput
+    .split('\n')
+    .map(b => b.trim())
+    .filter(Boolean);
+  return difference(branches, mergedBranches);
+}
+
+async function getConfigPath(sg) {
+  const repoRootPath = (await sg.raw(['rev-parse', '--show-toplevel'])).trim();
+  return `${repoRootPath}/.pr-train.yml`;
 }
 
 /**
@@ -66,167 +73,180 @@ async function getUnmergedBranches(sg, branches) {
  * @return {Promise.<{trains: Array.<TrainCfg>}>}
  */
 async function loadConfig(sg) {
-    const repoRootPath = (await sg.raw(['rev-parse', '--show-toplevel'])).trim();
-    return yaml.safeLoad(fs.readFileSync(`${repoRootPath}/.pr-train.yml`, 'utf8'));
+  const path = await getConfigPath(sg);
+  return yaml.safeLoad(fs.readFileSync(path, 'utf8'));
 }
 
 /**
  * @param {BranchCfg} branchCfg
  */
 function getBranchName(branchCfg) {
-    return typeof branchCfg === 'string' ? branchCfg : Object.keys(branchCfg)[0];
+  return typeof branchCfg === 'string' ? branchCfg : Object.keys(branchCfg)[0];
 }
 
 /**
  * @return {Promise.<Array.<BranchCfg>>}
  */
 async function getBranchesConfigInCurrentTrain(sg) {
-    const branches = await sg.branchLocal();
-    const currentBranch = branches.current;
-    const { trains } = await loadConfig(sg);
-    const key = Object.keys(trains).find(trainKey => {
-        const branches = trains[trainKey];
-        const branchNames = branches.map(b => getBranchName(b));
-        return branchNames.indexOf(currentBranch) >= 0;
-    });
-    return key && trains[key];
+  const branches = await sg.branchLocal();
+  const currentBranch = branches.current;
+  const { trains } = await loadConfig(sg);
+  const key = Object.keys(trains).find(trainKey => {
+    const branches = trains[trainKey];
+    const branchNames = branches.map(b => getBranchName(b));
+    return branchNames.indexOf(currentBranch) >= 0;
+  });
+  return key && trains[key];
 }
 
 /**
  * @param {Array.<BranchCfg>} branchConfig
  */
 function getBranchesInCurrentTrain(branchConfig) {
-    return branchConfig.map(b => getBranchName(b));
+  return branchConfig.map(b => getBranchName(b));
 }
 
 /**
  * @param {Array.<BranchCfg>} branchConfig
  */
 function getCombinedBranch(branchConfig) {
-    const combinedBranch = /** @type {Object<string, {combined: boolean}>} */ branchConfig.find(cfg => {
-        if (typeof cfg === 'string') {
-            return false;
-        }
-        const branchName = Object.keys(cfg)[0];
-        return cfg[branchName].combined;
-    });
-    if (!combinedBranch) {
-        return undefined;
+  const combinedBranch = /** @type {Object<string, {combined: boolean}>} */ branchConfig.find(cfg => {
+    if (typeof cfg === 'string') {
+      return false;
     }
-    const branchName = Object.keys(combinedBranch)[0];
-    return branchName;
+    const branchName = Object.keys(cfg)[0];
+    return cfg[branchName].combined;
+  });
+  if (!combinedBranch) {
+    return undefined;
+  }
+  const branchName = Object.keys(combinedBranch)[0];
+  return branchName;
 }
 
 async function handleSwitchToBranchCommand(sg, sortedBranches, combinedBranch) {
-    const switchToBranchIndex = program.args[0];
-    if (typeof switchToBranchIndex === 'undefined') {
-        return;
-    }
-    let targetBranch;
-    if (switchToBranchIndex === 'combined') {
-        targetBranch = combinedBranch;
-    } else {
-        targetBranch = sortedBranches[switchToBranchIndex];
-    }
-    if (!targetBranch) {
-        console.log(`Could not find branch with index ${switchToBranchIndex}`.red);
-        process.exit(3);
-    }
-    await sg.checkout(targetBranch);
-    console.log(`Switched to branch ${targetBranch}`);
-    process.exit(0);
+  const switchToBranchIndex = program.args[0];
+  if (typeof switchToBranchIndex === 'undefined') {
+    return;
+  }
+  let targetBranch;
+  if (switchToBranchIndex === 'combined') {
+    targetBranch = combinedBranch;
+  } else {
+    targetBranch = sortedBranches[switchToBranchIndex];
+  }
+  if (!targetBranch) {
+    console.log(`Could not find branch with index ${switchToBranchIndex}`.red);
+    process.exit(3);
+  }
+  await sg.checkout(targetBranch);
+  console.log(`Switched to branch ${targetBranch}`);
+  process.exit(0);
 }
 
 async function main() {
-    program
-        .version(package.version)
-        .option('-p, --push', 'Push changes')
-        .option('--list', 'List branches in current train')
-        .option('-r, --rebase', 'Rebase branches rather than merging them')
-        .option('-f, --force', 'Force push to remote')
-        .option('--push-merged', 'Push all branches (inclusing those that have already been merged into master)')
-        .option('--remote <remote>', 'Set remote to push to. Defaults to "origin"')
-        .option('--no-quote', 'Do not print wise quote on exit')
-        .option('-c, --create-prs', 'Create GitHub PRs from your train branches');
+  program
+    .version(package.version)
+    .option('-p, --push', 'Push changes')
+    .option('--list', 'List branches in current train')
+    .option('-r, --rebase', 'Rebase branches rather than merging them')
+    .option('-f, --force', 'Force push to remote')
+    .option('--push-merged', 'Push all branches (inclusing those that have already been merged into master)')
+    .option('--remote <remote>', 'Set remote to push to. Defaults to "origin"')
+    .option('--no-quote', 'Do not print wise quote on exit')
+    .option('-c, --create-prs', 'Create GitHub PRs from your train branches')
+    .option('--init', 'Creates a .pr-train.yml file with an example configuration');
 
-    program.on('--help', () => {
-        console.log('');
-        console.log('  Switching branches:');
-        console.log('');
-        console.log('    $ `git pr-train <index>` will switch to branch with index 2');
-        console.log('');
-        console.log('  Creating GitHub PRs:');
-        console.log('');
-        console.log(
-            '    $ `git pr-train -p --create-prs` will create GH PRs for all branches in your train (with a "table of contents")'
-        );
-        console.log(
-            colors.italic(
-                `    Please note you'll need to create a \`\${HOME}/.pr-train\` file with your GitHub access token first.`
-            )
-        );
-        console.log('');
-    });
-
-    program.parse(process.argv);
-
-    if (program.createPrs && !readGHKey()) {
-        console.log(`"$HOME/.pr-train" not found. Please make sure file exists and contains your GitHub API key`.red);
-        process.exit(4);
-    }
-
-    const sg = simpleGit();
-    if (!(await sg.checkIsRepo())) {
-        console.log('Not a git repo'.red);
-        process.exit(1);
-    }
-
-    const { current: currentBranch, all: allBranches } = await sg.branchLocal();
-    const trainCfg = await getBranchesConfigInCurrentTrain(sg);
-    const sortedTrainBranches = getBranchesInCurrentTrain(trainCfg);
-    const combinedTrainBranch = getCombinedBranch(trainCfg);
-
-    if (combinedTrainBranch && !allBranches.includes(combinedTrainBranch)) {
-        const lastBranchBeforeCombined = sortedTrainBranches[sortedTrainBranches.length - 2];
-        await sg.raw(['branch', combinedTrainBranch, lastBranchBeforeCombined]);
-    }
-
-    handleSwitchToBranchCommand(sg, sortedTrainBranches, combinedTrainBranch);
-
-    console.log(`I've found these partial branches:`);
+  program.on('--help', () => {
+    console.log('');
+    console.log('  Switching branches:');
+    console.log('');
+    console.log('    $ `git pr-train <index>` will switch to branch with index 2');
+    console.log('');
+    console.log('  Creating GitHub PRs:');
+    console.log('');
     console.log(
-        sortedTrainBranches.map(b => ` -> ${b.green}${b === combinedTrainBranch ? ' (combined)' : ''}`).join('\n'),
-        '\n'
+      '    $ `git pr-train -p --create-prs` will create GH PRs for all branches in your train (with a "table of contents")'
     );
-    if (program.list) {
-        return;
-    }
+    console.log(
+      colors.italic(
+        `    Please note you'll need to create a \`\${HOME}/.pr-train\` file with your GitHub access token first.`
+      )
+    );
+    console.log('');
+  });
 
-    for (let i = 0; i < sortedTrainBranches.length - 1; ++i) {
-        await combineBranches(sg, program.rebase, sortedTrainBranches[i], sortedTrainBranches[i + 1]);
-        await sleep(MERGE_STEP_DELAY_MS);
-    }
+  program.parse(process.argv);
 
-    if (program.push || program.pushMerged) {
-        let branchesToPush = sortedTrainBranches;
-        if (!program.pushMerged) {
-            branchesToPush = await getUnmergedBranches(sg, sortedTrainBranches);
-            const branchDiff = difference(sortedTrainBranches, branchesToPush);
-            if (branchDiff.length > 0) {
-                console.log(`Not pushing already merged branches: ${branchDiff.join(', ')}`);
-            }
-        }
-        pushChanges(sg, branchesToPush, program.force, program.remote);
-    }
+  if (program.createPrs && !readGHKey()) {
+    console.log(`"$HOME/.pr-train" not found. Please make sure file exists and contains your GitHub API key`.red);
+    process.exit(4);
+  }
 
-    if (program.createPrs) {
-        await ensurePrsExist(sg, sortedTrainBranches, combinedTrainBranch, program.remote);
-    }
+  const sg = simpleGit();
+  if (!(await sg.checkIsRepo())) {
+    console.log('Not a git repo'.red);
+    process.exit(1);
+  }
 
-    await sg.checkout(currentBranch);
+  if (program.init) {
+    if (fs.existsSync(await getConfigPath(sg))) {
+      console.log('.pr-train.yml already exists');
+      process.exit(1);
+    }
+    const root = path.dirname(require.main.filename);
+    const cfgTpl = fs.readFileSync(`${root}/cfg_template.yml`);
+    fs.writeFileSync(await getConfigPath(sg), cfgTpl);
+    console.log(`Created a ".pr-train.yml" file. Please make sure it's gitignored.`);
+    process.exit(0);
+  }
+
+  const { current: currentBranch, all: allBranches } = await sg.branchLocal();
+  const trainCfg = await getBranchesConfigInCurrentTrain(sg);
+  const sortedTrainBranches = getBranchesInCurrentTrain(trainCfg);
+  const combinedTrainBranch = getCombinedBranch(trainCfg);
+
+  if (combinedTrainBranch && !allBranches.includes(combinedTrainBranch)) {
+    const lastBranchBeforeCombined = sortedTrainBranches[sortedTrainBranches.length - 2];
+    await sg.raw(['branch', combinedTrainBranch, lastBranchBeforeCombined]);
+  }
+
+  handleSwitchToBranchCommand(sg, sortedTrainBranches, combinedTrainBranch);
+
+  console.log(`I've found these partial branches:`);
+  console.log(
+    sortedTrainBranches.map(b => ` -> ${b.green}${b === combinedTrainBranch ? ' (combined)' : ''}`).join('\n'),
+    '\n'
+  );
+  if (program.list) {
+    return;
+  }
+
+  for (let i = 0; i < sortedTrainBranches.length - 1; ++i) {
+    await combineBranches(sg, program.rebase, sortedTrainBranches[i], sortedTrainBranches[i + 1]);
+    await sleep(MERGE_STEP_DELAY_MS);
+  }
+
+  if (program.push || program.pushMerged) {
+    let branchesToPush = sortedTrainBranches;
+    if (!program.pushMerged) {
+      branchesToPush = await getUnmergedBranches(sg, sortedTrainBranches);
+      const branchDiff = difference(sortedTrainBranches, branchesToPush);
+      if (branchDiff.length > 0) {
+        console.log(`Not pushing already merged branches: ${branchDiff.join(', ')}`);
+      }
+    }
+    pushChanges(sg, branchesToPush, program.force, program.remote);
+  }
+
+  if (program.createPrs) {
+    await ensurePrsExist(sg, sortedTrainBranches, combinedTrainBranch, program.remote);
+  }
+
+  await sg.checkout(currentBranch);
 }
 
 main().catch(e => {
-    console.log(`${emoji.get('x')}  An error occured. Was there a conflict perhaps?`.red);
-    console.error('error', e);
+  console.log(`${emoji.get('x')}  An error occured. Was there a conflict perhaps?`.red);
+  console.error('error', e);
 });
